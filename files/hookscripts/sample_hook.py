37a483fa7ebb75a064eed786864751b6f14468c2
Matthias Schmitz <matthias@sigxcpu.org>
2013-11-09 22:18:49 +0100

    Add a sample hook to files/hooksscripts.
    * As git don't track empty directories 'hookscripts' isn't in the checkout and Puppet fails with
      "Could not retrieve information from environment production source(s) puppet:///modules/one/hookscripts"

---
+++ files/hookscripts/sample_hook.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf8
+
+# This is a sample hook which does nothing.
+# Please see the documentation of OpenNebula hooks
+#   http://opennebula.org/documentation:rel4.2:hooks
+
+def do_nothing():
+    pass
+
+if __name__ == '__main__':
+    do_nothing()
