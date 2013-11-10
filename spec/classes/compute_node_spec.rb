94594c9ca584771488ce3040b4e372f59d200d31
Martin Alfke <martin.alfke@epost-dev.de>
2013-11-10 15:02:11 +0100

    spec cleanup for Debian

--- spec/classes/compute_node_spec.rb
+++ spec/classes/compute_node_spec.rb
@@ -45,16 +45,15 @@ describe 'one::compute_node' do
             hiera = Hiera.new(:config => hiera_config)
             sshprivkey = hiera.lookup("one::node::ssh_priv_key", nil, nil)
             sshpubkey = hiera.lookup("one::node::ssh_pub_key", nil, nil)
-            it { should contain_package("opennebula-node-kvm") }
+            it { should contain_package("opennebula-node") }
             it { should contain_package("qemu-kvm") }
-            it { should contain_package("libvirt") }
+            it { should contain_package("libvirt-bin") }
             it { should contain_package("bridge-utils") }
-            it { should contain_package("vconfig") }
             it { should contain_package("sudo") }
             it { should contain_group("oneadmin") }
             it { should contain_user("oneadmin") }
             it { should contain_file("/etc/libvirt/libvirtd.conf") }
-            it { should contain_file("/etc/default/libvirtd") }
+            it { should contain_file("/etc/default/libvirt-bin") }
             it { should contain_file("/var/lib/one/.ssh/id_dsa")\
                 .with_content(sshprivkey)
             }
