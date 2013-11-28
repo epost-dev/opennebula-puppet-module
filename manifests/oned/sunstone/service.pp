0f09e2de31d0d663b6633bff71a4aba28bf9ca94
Martin Alfke <martin.alfke@epost-dev.de>
2013-11-28 13:35:20 +0100

    add sunstone service

---
+++ manifests/oned/sunstone/service.pp
@@ -0,0 +1,22 @@
+# == Class one::oned::sunstone::service
+#
+# Installation and Configuration of OpenNebula
+# http://opennebula.org/
+#
+# === Author
+# ePost Development GmbH
+# (c) 2013
+#
+# Contributors:
+# - Martin Alfke
+#
+# === License
+# Apache License Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0.html
+#
+class one::oned::sunstone::service {
+  service { 'opennebula-sunstone':
+    ensure => running,
+    enable => true,
+  }
+}
