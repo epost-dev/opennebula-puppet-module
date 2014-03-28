16a565273809d35a75aa053b584d4fded3f102b8
rwaffen <robert.waffen@epost-dev.de>
2014-03-28 15:16:22 +0100

    Add VMHook for Opennebula for puppet cert removal

--- manifests/oned/config.pp
+++ manifests/oned/config.pp
@@ -32,7 +32,8 @@ class one::oned::config(
   $backup_db_password = $one::params::backup_db_password,
   $backup_db_host     = $one::params::backup_db_host,
   $backup_intervall   = $one::params::backup_intervall,
-  $backup_keep        = $one::params::backup_keep
+  $backup_keep        = $one::params::backup_keep,
+  $puppet_hook        = $one::params::puppet_hook
   ) {
 
   File {
