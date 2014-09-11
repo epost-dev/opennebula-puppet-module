0a20e61e5919427dc3263aa3b697a14ef4659d74
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-09-11 14:55:19 +0200

    Use @property_hash and prefetch in oneimage's oneimage provider

--- lib/puppet/provider/oneimage/oneimage.rb
+++ lib/puppet/provider/oneimage/oneimage.rb
@@ -33,31 +33,19 @@ EOF
     file.close
     output = "oneimage create -d #{resource[:datastore]} #{file.path} ", self.class.login
     `#{output}`
+    @property_hash[:ensure] = :present
   end
 
   # Destroy a network using onevnet delete
   def destroy
     output = "oneimage delete #{resource[:name]} ", self.class.login
     `#{output}`
-  end
-
-  # Return a list of existing networks using the onevnet -x list command
-  def self.oneimage_list
-    output = "oneimage list --xml ", login
-    xml = REXML::Document.new(`#{output}`)
-    oneimages = []
-    xml.elements.each("IMAGE_POOL/IMAGE/NAME") do |element|
-      oneimages << element.text
-    end
-    oneimages
+    @property_hash.clear
   end
 
   # Check if a network exists by scanning the onevnet list
   def exists?
-    if self.class.oneimage_list().include?(resource[:name])
-        self.debug "Found image #{resource[:name]}"
-        true
-    end
+    @property_hash[:ensure] == :present
   end
 
   # Return the full hash of all existing oneimage resources
@@ -82,7 +70,15 @@ EOF
         :type        => ('context' if (elements["TEMPLATE/TYPE"] || elements["TYPE"]).text == '5')
       )
     end
+  end
 
+  def self.prefetch(resources)
+    images = instances
+    resources.keys.each do |name|
+      if provider = images.find{ |image| image.name == name }
+        resources[name].provider = provider
+      end
+    end
   end
 
   # login credentials
@@ -92,135 +88,6 @@ EOF
     password = credentials[1]
     login = " --user #{user} --password #{password}"
     login
-  end
-
-  # getters
-  def datastore
-      result = ''
-      output = "oneimage show #{resource[:name]} --xml ", self.class.login
-      xml = REXML::Document.new(`#{output}`)
-      # Traverse the XML document and populate the common attributes
-      xml.elements.each("IMAGE/DATASTORE") { |element|
-        result = element.text
-      }
-      result
-  end
-  def description
-      result = ''
-      output = "oneimage show #{resource[:name]} --xml ", self.class.login
-      xml = REXML::Document.new(`#{output}`)
-      xml.elements.each("IMAGE/TEMPLATE/DESCRIPTION") { |element|
-       result = element.text
-      }
-      result
-  end
-  def type
-      result = ''
-      output = "oneimage show #{resource[:name]} --xml ", self.class.login
-      xml = REXML::Document.new(`#{output}`)
-      xml.elements.each("IMAGE/TYPE") { |element|
-        case element.text
-        when '5'
-            result = 'context'
-        end
-      }
-      xml.elements.each("IMAGE/TEMPLATE/TYPE") { |element|
-        case element.text
-        when '5'
-            result = 'context'
-        end
-      }
-      result
-  end
-  def persistent
-      result = ''
-      output = "oneimage show #{resource[:name]} --xml ", self.class.login
-      xml = REXML::Document.new(`#{output}`)
-      xml.elements.each("IMAGE/PERSISTENT") { |element|
-        result = element.text == "1" ? true : false
-      }
-      xml.elements.each("IMAGE/TEMPLATE/PERSISTENT") { |element|
-        result = element.text == "1" ? true : false
-      }
-      result
-  end
-  def dev_prefix
-      result = ''
-      output = "oneimage show #{resource[:name]} --xml ", self.class.login
-      xml = REXML::Document.new(`#{output}`)
-      xml.elements.each("IMAGE/TEMPLATE/DEV_PREFIX") { |element|
-        result = element.text
-      }
-      result
-  end
-  def target
-      result = ''
-      output = "oneimage show #{resource[:name]} --xml ", self.class.login
-      xml = REXML::Document.new(`#{output}`)
-      xml.elements.each("IMAGE/TARGET") { |element|
-        result = element.text
-      }
-      result
-  end
-  def path
-      result = ''
-      output = "oneimage show #{resource[:name]} --xml ", self.class.login
-      xml = REXML::Document.new(`#{output}`)
-      xml.elements.each("IMAGE/PATH") { |element|
-        result = element.text
-      }
-      xml.elements.each("IMAGE/TEMPLATE/PATH") { |element|
-        result = element.text
-      }
-      result
-  end
-  def driver
-      result = ''
-      output = "oneimage show #{resource[:name]} --xml ", self.class.login
-      xml = REXML::Document.new(`#{output}`)
-      xml.elements.each("IMAGE/DRIVER") { |element|
-        result = element.text
-      }
-      result
-  end
-  def disk_type
-      result = ''
-      output = "oneimage show #{resource[:name]} --xml ", self.class.login
-      xml = REXML::Document.new(`#{output}`)
-      xml.elements.each("IMAGE/DISK_TYPE") { |element|
-        result = element.text
-      }
-      result
-  end
-  def source
-      result = ''
-      output = "oneimage show #{resource[:name]} --xml ", self.class.login
-      xml = REXML::Document.new(`#{output}`)
-      xml.elements.each("IMAGE/SOURCE") { |element|
-        result = element.text
-      }
-      xml.elements.each("IMAGE/TEMPLATE/SOURCE") { |element|
-        result = element.text
-      }
-      result
-  end
-  def size
-      result = ''
-      output = "oneimage show #{resource[:name]} --xml ", self.class.login
-      xml = REXML::Document.new(`#{output}`)
-      xml.elements.each("IMAGE/SIZE") { |element|
-        result = element.text
-      }
-      result
-  end
-  def fstype
-      result = ''
-      output = "oneimage show #{resource[:name]} --xml ", self.class.login
-      xml = REXML::Document.new(`#{output}`)
-      xml.elements.each("IMAGE/FSTYPE") { |element|
-        result = element.text
-      }
-      result
   end
 
   #setters
