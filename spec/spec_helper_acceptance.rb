24fbbbbdb4ea4bc0d2fad6dd168ce1136f5e21b9
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-09-12 11:32:44 +0200

    Move hiera configuration to 'before suite' section

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -6,10 +6,6 @@ hosts.each do |host|
   install_package host, 'rubygems'
   on host, 'gem install puppet --no-ri --no-rdoc'
   on host, "mkdir -p #{host['distmoduledir']}"
-  # Configure hiera
-  on host, "touch /etc/puppet/hiera.yaml"
-  on host, "mkdir -p /var/lib/hiera"
-  on host, "echo -e '---\none::enable_opennebula_repo: true\none::ha_setup: false\n' > /var/lib/hiera/common.yaml"
 end
 
 RSpec.configure do |c|
@@ -24,6 +20,12 @@ RSpec.configure do |c|
     # Install module
     puppet_module_install(:source => proj_root, :module_name => 'one')
     hosts.each do |host|
+      # Configure hiera
+      on host, "/bin/touch #{default['puppetpath']}/hiera.yaml"
+      on host, "mkdir -p /var/lib/hiera"
+      on host, "echo -e '---\none::enable_opennebula_repo: true\none::ha_setup: false\n' > /var/lib/hiera/common.yaml"
+
+      # Install dependencies
       on host, puppet('module','install','puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module','install','puppetlabs-apt'), { :acceptable_exit_codes => [0,1] }
     end
