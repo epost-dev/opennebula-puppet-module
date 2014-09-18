c41eec3d56efa06a4383c94f411a000aaaadbbde
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-09-18 14:59:18 +0200

    Add acceptance tests for oneimage

--- spec/acceptance/oneimage_spec.rb
+++ spec/acceptance/oneimage_spec.rb
@@ -1,21 +1,30 @@
 require 'spec_helper_acceptance'
 
 describe 'oneimage type' do
+  before :all do
+    pp = <<-EOS
+    class { 'one':
+      oned => true,
+    }
+    file { ['/home/one_user', '/home/one_user/images']:
+      ensure => directory,
+    }
+    EOS
+    apply_manifest(pp, :catch_failures => true)
+    apply_manifest(pp, :catch_changes => true)
+  end
 
-  describe 'when creating an image' do
+  describe 'when creating an OS image' do
     it 'should idempotently run' do
-      pending 'Need fix'
       pp = <<-EOS
-        class { 'one':
-          oned => true,
-        } ->
-        oneimage { 'new_image':
-          source => '/foo/bar.img',
-          size   => '50',
-        } ->
-        oneimage { 'new_image2':
-          source => '/foo/baz.img',
-          size   => '100',
+        exec { '/usr/bin/qemu-img create /home/one_user/images/ubuntu_desktop.img 1G':
+          creates => '/home/one_user/images/ubuntu_desktop.img',
+        }
+        ->
+        oneimage { 'Ubuntu':
+          datastore   => 'default',
+          description => 'Ubuntu 10.04 desktop for students.',
+          path        => '/home/one_user/images/ubuntu_desktop.img',
         }
       EOS
 
@@ -24,11 +33,75 @@ describe 'oneimage type' do
     end
   end
 
-  describe 'when destroying an image' do
+  describe 'when creating a CDROM image' do
+    it 'should idempotently run' do
+      pp = <<-EOS
+        exec { '/usr/bin/mkisofs -o /home/one_user/images/matlab.iso /tmp':
+          creates => '/home/one_user/images/matlab.iso',
+        }
+        ->
+        oneimage { 'MATLAB install CD':
+          datastore   => 'default',
+          description => 'Contains the MATLAB installation files. Mount it to install MATLAB on new OS images.',
+          path        => '/home/one_user/images/matlab.iso',
+          type        => 'cdrom',
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+  end
+
+  describe 'when creating a DATABLOCK image' do
+    it 'should idempotently run' do
+      pending 'This example from the doc does not actually work!'
+      pp = <<-EOS
+        oneimage { 'Experiment results':
+          datastore   => 'default',
+          description => 'Storage for my Thesis experiments.',
+          size        => '3.08',
+          fstype      => 'ext3',
+          type        => 'datablock',
+        }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+  end
+
+  describe 'when destroying an OS image' do
     it 'should idempotently run' do
       pp =<<-EOS
-      oneimage { 'new_image2':
-       ensure => absent,
+      oneimage { 'Ubuntu':
+        ensure => absent,
+      }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+  end
+
+  describe 'when destroying a CDROM image' do
+    it 'should idempotently run' do
+      pp =<<-EOS
+      oneimage { 'MATLAB install CD':
+        ensure => absent,
+      }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+  end
+
+  describe 'when destroying a DATABLOCK image' do
+    it 'should idempotently run' do
+      pp =<<-EOS
+      oneimage { 'Experiment results':
+        ensure => absent,
       }
       EOS
 
