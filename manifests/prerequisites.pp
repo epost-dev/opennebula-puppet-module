9db729e257df4fd3dd0bf7cf914a0bce18fcfc1f
Choon Ming Goh <choon.ming@choonming.org>
2014-12-04 00:31:32 +0100

    update yum repo URL to the latest version

--- manifests/prerequisites.pp
+++ manifests/prerequisites.pp
@@ -23,7 +23,7 @@ class one::prerequisites {
         'RedHat': {
             if ( $one::params::one_repo_enable == true ) {
                 yumrepo { 'opennebula':
-                    baseurl  => 'http://downloads.opennebula.org/repo/4.10/CentOS/6/stable/$basearch/',
+                    baseurl  => 'http://downloads.opennebula.org/repo/4.10/CentOS/6/x86_64/',
                     descr    => 'OpenNebula',
                     enabled  => 1,
                     gpgcheck => 0,
