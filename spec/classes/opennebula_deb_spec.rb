73c4cd90c0c169ea001b5c2f02e2714543836e85
Martin Alfke <martin.alfke@buero20.org>
2015-01-28 12:51:37 +0100

    adopt spec test to new oned.conf

--- spec/classes/opennebula_deb_spec.rb
+++ spec/classes/opennebula_deb_spec.rb
@@ -157,7 +157,7 @@ describe 'one' do
           it { should contain_file(oned_config).with_content(/^#{expected_host_hook}/m) }
         end
         context 'with xmlrpc tuning' do
-          it { should contain_file('/etc/one/oned.conf').with_content(/MAX_CONN = 5000/) }
+          it { should contain_file('/etc/one/oned.conf').with_content(/MAX_CONN           = 5000/) }
         end
         context 'with default hook scripts rolled out' do
           it { should contain_file('/usr/share/one/hooks').with_source('puppet:///modules/one/hookscripts') }
