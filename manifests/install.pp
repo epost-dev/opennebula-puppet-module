2282b29399ef0be666a7e99ade3e873003dfb429
Robert Waffen <robert.waffen@epost-dev.de>
2015-03-17 14:12:29 +0100

    refactor one::install
    - make class parameterized
    - add spec test only for this class

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,16 +2,19 @@
 #
 # packages needed by both (compute node and head)
 #
-class one::install {
+class one::install (
+  $http_proxy = $one::params::http_proxy,
+  $dbus_pkg   = $one::params::dbus_pkg,
+){
 
   file { '/root/.gemrc':
     ensure  => 'file',
-    content => "---\nhttp_proxy: ${one::params::http_proxy}\n"
+    content => "---\nhttp_proxy: ${http_proxy}\n",
   }
 
   File['/root/.gemrc'] -> Package <| provider == "gem" |>
 
-  package { $one::params::dbus_pkg:
+  package { $dbus_pkg:
     ensure  => present,
     require => Class['one::prerequisites'],
   }
