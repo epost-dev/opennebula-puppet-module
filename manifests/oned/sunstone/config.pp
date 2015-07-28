f86d44cb35a830347c7169fc87d900978be23349
Justin Riley <justin.t.riley@gmail.com>
2015-07-28 12:14:01 -0400

    add support for setting custom logos in sunstone

--- manifests/oned/sunstone/config.pp
+++ manifests/oned/sunstone/config.pp
@@ -17,16 +17,18 @@
 # http://www.apache.org/licenses/LICENSE-2.0.html
 #
 class one::oned::sunstone::config (
-  $listen_ip             = $one::sunstone_listen_ip,
-  $enable_support        = $one::enable_support,
-  $enable_marketplace    = $one::enable_marketplace,
-  $tmpdir                = $one::sunstone_tmpdir,
-  $sessions              = $one::sunstone_sessions,
-  $vnc_proxy_port        = $one::vnc_proxy_port,
-  $vnc_proxy_support_wss = $one::vnc_proxy_support_wss,
-  $vnc_proxy_cert        = $one::vnc_proxy_cert,
-  $vnc_proxy_key         = $one::vnc_proxy_key,
-  $vnc_proxy_ipv6        = $one::vnc_proxy_ipv6,
+  $listen_ip               = $one::sunstone_listen_ip,
+  $enable_support          = $one::enable_support,
+  $enable_marketplace      = $one::enable_marketplace,
+  $tmpdir                  = $one::sunstone_tmpdir,
+  $sessions                = $one::sunstone_sessions,
+  $vnc_proxy_port          = $one::vnc_proxy_port,
+  $vnc_proxy_support_wss   = $one::vnc_proxy_support_wss,
+  $vnc_proxy_cert          = $one::vnc_proxy_cert,
+  $vnc_proxy_key           = $one::vnc_proxy_key,
+  $vnc_proxy_ipv6          = $one::vnc_proxy_ipv6,
+  $sunstone_logo_png       = $one::sunstone_logo_png,
+  $sunstone_logo_small_png = $one::sunstone_logo_small_png,
 ){
   File {
     owner   => 'root',
@@ -47,5 +49,35 @@ class one::oned::sunstone::config (
     ensure  => file,
     mode    => '0640',
     content => template('one/sunstone-views.yaml.erb'),
+  } ->
+  file { '/etc/one/sunstone-views/admin.yaml':
+    ensure  => file,
+    mode    => '0640',
+    content => template('one/sunstone-views-admin.yaml.erb'),
+  }
+
+  if $sunstone_logo_png != 'undef' or $sunstone_logo_small_png != 'undef' {
+    file { '/usr/lib/one/sunstone/public/images':
+      ensure  => directory,
+      mode    => '0755',
+    }
+  }
+
+  if $sunstone_logo_png != 'undef' {
+    file { '/usr/lib/one/sunstone/public/images/custom_logo.png':
+      ensure  => file,
+      mode    => '0644',
+      source  => $sunstone_logo_png,
+      require => File['/usr/lib/one/sunstone/public/images'],
+    }
+  }
+
+  if $sunstone_logo_small_png != 'undef' {
+    file { '/usr/lib/one/sunstone/public/images/custom_logo_small.png':
+      ensure  => file,
+      mode    => '0644',
+      source  => $sunstone_logo_small_png,
+      require => File['/usr/lib/one/sunstone/public/images'],
+    }
   }
 }
