84faf166478fffde2e728aac48d6935b42f74bdf
John Noss <noss@harvard.edu>
2017-02-16 15:13:13 -0500

    Revert "temporarily add debug for vnet provider"
    This reverts commit 9e1244a7db4c0f53cfaaa527e7d8e8ce840e70a7.

--- lib/puppet/provider/onevnet/cli.rb
+++ lib/puppet/provider/onevnet/cli.rb
@@ -47,8 +47,7 @@ Puppet::Type.type(:onevnet).provide(:cli) do
     file.close
     self.debug "Adding new network using template: #{tempfile}"
     onevnet('create', file.path)
-    self.debug "new network temp file path: #{file.path}"
-    #file.delete
+    file.delete
     @property_hash[:ensure] = :present
   end
 
@@ -119,7 +118,6 @@ Puppet::Type.type(:onevnet).provide(:cli) do
     file.close
     self.debug(IO.read file.path)
     onevnet('update', resource[:name], file.path, '--append') unless @property_hash.empty?
-    self.debug "updating network from file path: #{file.path}"
-    #file.delete
+    file.delete
   end
 end
