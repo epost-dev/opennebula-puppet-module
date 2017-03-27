9354df1b09ee0f402c59804024a0ad48f8302f9d
Justin Riley <justin.t.riley@gmail.com>
2017-03-27 11:47:25 -0400

    ensure oneflow config is owned by oneadmin

--- manifests/oned/oneflow/config.pp
+++ manifests/oned/oneflow/config.pp
@@ -33,6 +33,8 @@ class one::oned::oneflow::config (
   file { '/etc/one/oneflow-server.conf':
     ensure  => file,
     mode    => '0640',
+    owner   => 'oneadmin',
+    group   => 'oneadmin',
     content => template('one/oneflow-server.conf.erb'),
   }
 }
