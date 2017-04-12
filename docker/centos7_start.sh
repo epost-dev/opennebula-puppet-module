a32bfc8a938f4cb3a3bbd9e7de374098cdd98248
John Noss <john.noss@gmail.com>
2017-04-12 19:32:23 -0400

    updates to docker startup script

--- docker/centos7_start.sh
+++ docker/centos7_start.sh
@@ -3,10 +3,24 @@
 date
 echo running puppet apply $1
 time puppet apply $1
+echo making ssh host keys
+/usr/bin/ssh-keygen -A
+echo starting ssh
+/usr/sbin/sshd -E /var/log/sshd.log
+echo starting libvirt
+source /etc/sysconfig/libvirtd
+/usr/sbin/libvirtd -d $LIBVIRTD_ARGS
+echo starting dbus
+mkdir -p /var/run/dbus
+/bin/dbus-daemon --system --nopidfile
 echo starting oned
 su - oneadmin -c /usr/bin/oned
+echo starting scheduler
+su - oneadmin -c /usr/bin/mm_sched &
 echo sleeping 10
 sleep 10
+echo adding onehost
+onehost create localhost -i kvm -v kvm
 echo starting sunstone
 su - oneadmin -c "/usr/bin/sunstone-server start"
 echo done
